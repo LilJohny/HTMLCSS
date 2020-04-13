3434b38fa6d091203ff12bbe4e00fa6c2de814b7
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-04-13 16:15:12 +0300

    Started implementing navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,25 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
     eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('img');
+
+    eleventyConfig.setDataDeepMerge(true);
+
 
     return {
         dir: {
             input: "html",
             output: "dist",
+            includes:"_includes"
         },
-        templateFormats: ["html","njk","css"],
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        templateFormats: ["html", "njk", "css"],
         passthroughFileCopy: true,
         templateEngineOverride: "njk"
     };

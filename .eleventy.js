4468d0e91f200ee71254b5f371586d79b66b9358
Denis Ivanenko <ivanenko@ucu.edu.ua>
2020-04-13 21:09:07 +0300

    A lot of fixes

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,8 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-    eleventyConfig.addPassthroughCopy('css');
-    eleventyConfig.addPassthroughCopy('img');
+    eleventyConfig.addPassthroughCopy('src/css');
+    eleventyConfig.addPassthroughCopy('src/img');
 
     eleventyConfig.setDataDeepMerge(true);
 
